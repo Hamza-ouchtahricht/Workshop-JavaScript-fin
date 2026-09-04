edb1f578b017e8a5ed3a24872392cb890a4e5911
Hamza-ouchtahricht <fourtiying@gmail.com>
2026-09-03 20:53:18 -0700

    Implement min function and simplify comparisons
    Refactored code to create a min function and simplify logic.

--- fonctions/Challenge4.js
+++ fonctions/Challenge4.js
@@ -4,26 +4,12 @@ var a=parseInt(prompt("entre le nomber a:"));
 
 var b=parseInt(prompt("entre le nomber b:"));
 
-if(a>b){
-        function max(a,b){
-
-                return a;}
-
-        var result=max(a,b)
-
-        console.log("b le min");
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else if(a>b) {
+        return b;
+    }
 }
-
-else if(a<b){
-
-        function max(a,b){
-
-                return b;}
-        var result=max(a,b)
-
-        console.log("a le min");
-}
-else{
-
-        console.log("a=b");
-}
+let result=min(a,b);
+console.log(`le min est ${result}`);
