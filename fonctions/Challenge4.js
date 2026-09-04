846a07a0411c4ff3e07e79977a208094a49bf762
Hamza-ouchtahricht <fourtiying@gmail.com>
2026-09-03 20:54:35 -0700

    Change var to let for variable declarations

--- fonctions/Challenge4.js
+++ fonctions/Challenge4.js
@@ -1,8 +1,8 @@
 const prompt=require('prompt-sync')();
 
-var a=parseInt(prompt("entre le nomber a:"));
+let a=parseInt(prompt("entre le nomber a:"));
 
-var b=parseInt(prompt("entre le nomber b:"));
+let b=parseInt(prompt("entre le nomber b:"));
 
 function min(a, b) {
     if (a < b) {
